dcb722212d64354a6f1f2e20135980624f1d152f
RevetE <Esther.Revet@auxitec.arteliagroup.com>
2018-03-06 11:51:12 +0100

    Ajout date

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,5 @@
 <header>
-    <nav> // menu
+    <nav> <!-- instruction Menu -->
         <li><a href="index.php?page=acceuil"> Acceuil</a> </li>
         <li><a href="index.php?page=inscription"> Inscription</a> </li>
         <li><a href="index.php?page=login"> Login</a> </li>
