323d437e9b9e85f80f6a5de5e4ba0bcdac5bee24
RevetE <Esther.Revet@auxitec.arteliagroup.com>
2018-03-06 13:44:28 +0100

    Page aotomaitques

--- index.php
+++ index.php
@@ -3,5 +3,37 @@ include "./includes/header.php";
 
 echo "<h1> Ceci est le corp de mon index.PHP </h1>";
 
+if (isset($_GET['page']) && $_GET['page'] != "")
+    {
+    $page = $_GET['page'];
+    }
+else
+    {
+     $page = "acceuil";
+    }
+
+// Opérateur ternaire
+$page = (isset($_GET['page']) && $_GET['page'] != "") ? $_GET['page'] : "acceuil";
+
+$page = "./includes/" . $page . ".inc.php";
+
+var_dump($page); // affiche la variable : son type et sa valeur
+
+$files = glob("./includes/*.inc.php"); // ramène liste de tous les répertoires inc.php
+
+echo"<pre>"; // concerve la présentation avec les entrée
+  print_r ($files); // print_r => comme echo mais bon pour afficher les tableaux
+echo"</pre>";
+
+if (in_array($page,$files))
+    {
+    include $page;
+    }
+else
+    {
+        include "./includes/acceuil.inc.php";
+    }
+
+
 include "./includes/footer.php";
 
